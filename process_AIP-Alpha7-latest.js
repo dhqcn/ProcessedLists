ba184b3b839f44fa65c76be47f8a9f22aae5cd75
dhqcn <130745977+dhqcn@users.noreply.github.com>
2024-04-28 23:31:41 +1000

    Create process_AIP-Alpha7-latest.js

---
+++ process_AIP-Alpha7-latest.js
@@ -0,0 +1,11 @@
+const fs = require("fs").promises;
+
+(async () => {
+
+const file = await fs.readFile("AIP-Alpha7-latest.csv","utf8");
+
+const filtered = file.replaceAll(/ attacker, "");
+  
+await fs.writeFile("processed_AIP-Alpha7-latest.txt", filtered, "utf8");
+  
+})();
