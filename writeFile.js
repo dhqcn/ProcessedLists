27940840cfcae841f31d5e28af46dee441b8ac8b
dhqcn <130745977+dhqcn@users.noreply.github.com>
2024-04-28 23:18:53 +1000

    Update writeFile.js

--- writeFile.js
+++ writeFile.js
@@ -1,20 +1,10 @@
 const fs = require("fs");
 const https = require("https");
 
-const file = fs.createWriteStream("drop.txt");
+const file = fs.createWriteStream("AIP-Alpha7-latest.csv");
 
-https.get("https://www.spamhaus.org/drop/drop.txt", response => {
+https.get("https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Latest/AIP-Alpha7-latest.csv", response => {
   var stream = response.pipe(file);
-
-  stream.on("finish", function() {
-    console.log("done");
-  });
-});
-
-const file2 = fs.createWriteStream("dshield.txt");
-
-https.get("https://www.dshield.org/block.txt", response => {
-  var stream = response.pipe(file2);
 
   stream.on("finish", function() {
     console.log("done");
