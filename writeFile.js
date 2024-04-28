ccf35d099dab11618e349ef04228a6e0ead89676
dhqcn <130745977+dhqcn@users.noreply.github.com>
2024-04-28 23:15:16 +1000

    Create writeFile.js

---
+++ writeFile.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const https = require("https");
+
+const file = fs.createWriteStream("drop.txt");
+
+https.get("https://www.spamhaus.org/drop/drop.txt", response => {
+  var stream = response.pipe(file);
+
+  stream.on("finish", function() {
+    console.log("done");
+  });
+});
+
+const file2 = fs.createWriteStream("dshield.txt");
+
+https.get("https://www.dshield.org/block.txt", response => {
+  var stream = response.pipe(file2);
+
+  stream.on("finish", function() {
+    console.log("done");
+  });
+});
