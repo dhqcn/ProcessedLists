0f63209f3ffe5749e12bcfee4cd45040f2b57ca5
dhqcn <130745977+dhqcn@users.noreply.github.com>
2024-04-29 01:24:08 +1000

    Create process_s_drop.js

---
+++ process_s_drop.js
@@ -0,0 +1,11 @@
+const fs = require("fs").promises;
+
+(async () => {
+
+const file = await fs.readFile("drop.txt","utf8");
+
+const filtered = file.replaceAll(/ ;.*/g, "");
+  
+await fs.writeFile("processed_drop.txt", filtered, "utf8");
+  
+})();
