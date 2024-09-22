db46a9cb0ab6586709142a6318a19259076f5226
dhqcn <130745977+dhqcn@users.noreply.github.com>
2024-09-22 23:09:58 +1000

    Update writeFile.js

--- writeFile.js
+++ writeFile.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const https = require("https");
 
 # const file = fs.createWriteStream("AIP-Alpha7-latest.csv");
-
+#
 # https.get("https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Latest/AIP-Alpha7-latest.csv", response => {
 #  var stream = response.pipe(file);
 #
