b6f5696f084fd672ea2a388a78909f94e7868c9d
dhqcn <130745977+dhqcn@users.noreply.github.com>
2024-09-23 14:00:35 +1000

    Update writeFile.js

--- writeFile.js
+++ writeFile.js
@@ -40,13 +40,3 @@ https.get("https://www.spamhaus.org/drop/dropv6.txt", response => {
     console.log("done");
   });
 });
-
-const file5 = fs.createWriteStream("AIP-Alpha7-latest.csv");
-
-https.get("https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Latest/AIP-Alpha7-latest.csv", response => {
-  var stream = response.pipe(file5);
-
-  stream.on("finish", function() {
-    console.log("done");
-  });
-});
